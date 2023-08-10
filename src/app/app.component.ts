b1935bae8f22bcf5c343f327240b07d4b783e6b8
dajhelyan <assiaxramx10@gmail.com>
2023-08-09 19:10:53 -0500

    Project set up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ftp-challenge';
+  changeTitle(){
+    this.title = "new title"
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FileComponent } from './components/file/file.component';
+import { TableComponent } from './components/table/table.component';
+import { EditPopupComponent } from './components/edit-popup/edit-popup.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card'
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FileComponent,
+    TableComponent,
+    EditPopupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatTableModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
